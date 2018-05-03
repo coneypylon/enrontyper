eecbe801831a2f86ab00840134d1edc2f34b1945
coneypylon <uthorrr@gmail.com>
2018-05-03 20:09:46 -0300

    Move emails from transfer folder to Emails

---
+++ EmailTools/transferemails.py
@@ -0,0 +1,23 @@
+import os
+import multiprocessing
+import shutil
+
+def flatreturn(directory):
+	'''moves files from the given directory, calls itself on lower directories.
+	
+	:param directory: A directory path
+	
+	:returns: None
+	
+	>>> flatreturn(/usr/):
+	
+	'''
+	filelst = os.listdir(directory)
+	
+	for file in filelst:
+		if os.path.isdir(directory + "\\" + file):
+			flatreturn(directory + "\\" + file)
+		else:
+			shutil.copy(directory + "\\" + file,"..\\Emails")
+	
+flatreturn("..\\TRANSFER")
