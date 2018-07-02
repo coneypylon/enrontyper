2ef04148b73de70ada3c007a0bc1aa4c69e01e76
coneypylon <uthorrr@gmail.com>
2018-07-01 23:18:58 -0300

    Functional test()

--- EmailTools/main.py
+++ EmailTools/main.py
@@ -15,7 +15,7 @@ def clearscreen():
     except:
         os.system('clr')
 
-def test(str,FIRSTLINE=False):
+def test(string,FIRSTLINE=False,line=-1):
     '''displays the string, and waits for input. If the input is shorter than
     the string, keeps receiving input.
 
@@ -27,15 +27,25 @@ def test(str,FIRSTLINE=False):
     time in seconds for the string.
     '''
     if FIRSTLINE:
-        print("Type the following. Press Enter at the end of each line.")
+        print("Type the following. Press Enter at the end of each line.\n")
+    if line >= 0:
+        print("%s lines remain" % line)
 
     start = time.time()
 
-    test = input(str)
+    test = input(string + "\n")
 
-    while len(test) < len(str):
+    while len(test) < len(string):
         test += input()
+
     end = time.time()
+    incorrect = 0
+    # check the results
+    for character in range(0,len(string)):
+        if test[character] != string[character]:
+            incorrect += 1
+    return (incorrect,end - start)
+
 
 def login(namey,db="../DB/emails.db"):
     '''Logs in. For right now, it matches the namey in the database, and
@@ -93,11 +103,10 @@ def getrandemail(db="../DB/emails.db"):
         conn = sqlite3.connect(db)
         c = conn.cursor()
 
-        findMax = "SELECT * FROM EMAILS"
+        findMax = "SELECT MAX(EID) FROM EMAILS"
         c.execute(findMax)
 
-        t = c.fetchone()
-        print(t)
+        t = c.fetchone()[0]
         maxEID = int(t)
 
         EID = random.randint(1,maxEID)
@@ -110,6 +119,15 @@ def getrandemail(db="../DB/emails.db"):
     except Exception as e:
         print(str(e))
         sys.exit()
+
+def score(ttime,tmistakes,tchars):
+    '''Processes the total time, total mistakes, and total characters into
+    a score that amounts to your characters per minute minus your mistakes,
+    which are divided by the total number of characters.
+    '''
+    cpm = tchars/ttime * 60
+    acpm = cpm - tmistakes/tchars
+    return acpm
 
 def main(db="../DB/emails.db"):
     '''The main runtime. Runs the program when called, starts with a login,
@@ -128,10 +146,25 @@ def main(db="../DB/emails.db"):
         if curmode.lower() == "random":
             # test random email.
             curemail = getrandemail(db)
-            print(curemail)
+            lines = len(curemail[1].split("\n"))
+            totaltime = 0
+            totalmistakes = 0
+            totalchars = 0
+            first = True
+            for line in curemail[1].split("\n"):
+                if not line == "":
+                    line = line.strip()
+                    t = test(line,first,lines)
+                    first = False
+                    totaltime += t[1]
+                    totalmistakes += t[0]
+                    totalchars += len(line)
+                    lines -= 1
+            t = score(totaltime,totalmistakes,totalchars)
+            print("Your score was %s!" % t) # say more about thsi
 
         else:
-            raise NotImplementedError
+            RUNNING = False
 
 if __name__ == "__main__":
     main()
